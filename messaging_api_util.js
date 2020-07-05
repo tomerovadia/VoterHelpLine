e4c611ee96d862c3f160bde8e8f57b20336f13c7
Tomer Ovadia <tomertomertomer@gmail.com>
2020-07-05 11:04:46 -0400

    Delete messaging_api_util.js which intended to handle situations where a message is being sent to both Twilio and Slack but never ended up being put to use

--- messaging_api_util.js
+++
@@ -1,7 +0,0 @@
-const SlackApiUtil = require('./slack_api_util');
-const TwilioApiUtil = require('./twilio_api_util');
-
-exports.sendTwilioAndSlackMessage = (message, slackOptions, twilioOptions) => {
-  TwilioApiUtil.sendMessage(message, twilioOptions);
-  SlackApiUtil.sendMessage(message, slackOptions);
-}
