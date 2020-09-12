9291c1f8d82c5bea49491fd075d6ef79175fb795
Tomer Ovadia <tomertomertomer@gmail.com>
2020-09-12 12:43:01 -0400

    Clean up and add debug logs

--- load_balancer.js
+++ load_balancer.js
@@ -1,3 +1,5 @@
+const logDebug = process.env.NODE_ENV !== "test";
+
 const PULL_ENTRY_POINT = "PULL";
 const PUSH_ENTRY_POINT = "PUSH";
 exports.PULL_ENTRY_POINT = PULL_ENTRY_POINT;
@@ -24,37 +26,53 @@ exports.getPushPhoneNumberState = (twilioPhoneNumber) => {
 }
 
 exports.selectSlackChannel = (redisClient, entryPoint, stateName, isDemo = false) => {
+  if (logDebug) console.log("\n ENTERING LOADBALANCER.selectSlackChannel");
+  if (logDebug) console.log(`LOADBALANCER.selectSlackChannel: LoadBalancer given the following arguments: entryPoint: ${entryPoint}, stateName: ${stateName}, isDemo: ${isDemo}`);
   // If for some reason there's no stateName, Redis won't be able to provide
   // the needed info for determining a Slack channel. Caller should default
   // to #lobby.
-  if (!stateName) return null;
+  if (!stateName) {
+    if (logDebug) console.log("LOADBALANCER.selectSlackChannel: U.S. state not provided, LoadBalancer returning null.");
+    return null;
+  }
   const stateNameNoSpace = stateName.replace(/\s/g, '');
   const demoString = isDemo ? "Demo" : "";
   const entryPointString = entryPoint === PULL_ENTRY_POINT ? "Pull" : "Push";
 
   // Key to Redis values of number of voters must follow this format.
   const voterCounterKey = `voterCounter${entryPointString}${demoString}${stateNameNoSpace}`;
+  if (logDebug) console.log(`LOADBALANCER.selectSlackChannel: Determined voterCounterKey: ${voterCounterKey}`);
   // Keys to Redis lists of open pods must follow this format.
   const openPodsKey = `openPods${entryPointString}${demoString}${stateNameNoSpace}`;
-
-  console.log(`LoadBalancer: Checking Redis for ${voterCounterKey}.`)
-  console.log(`LoadBalancer: Checking Redis for ${openPodsKey}.`)
+  if (logDebug) console.log(`LOADBALANCER.selectSlackChannel: Determined openPodsKey: ${openPodsKey}`);
 
   return redisClient.getAsync(voterCounterKey).then((numVoters, err) => {
-    if (err || !numVoters) console.log(`LoadBalancer: Error! Couldn't find ${voterCounterKey} voterCounterKey in Redis:`, err);
+    if (err || !numVoters) if (logDebug) console.log(`LOADBALANCER.selectSlackChannel: ERROR finding voterCounterKey ${voterCounterKey} in Redis: err || !numVoters`);
+    if (logDebug) console.log(`LOADBALANCER.selectSlackChannel: Successfully found numVoters with voterCounterKey ${voterCounterKey} in Redis: ${numVoters}`);
     return redisClient.lrangeAsync(openPodsKey, 0, 1000 /* max # of pods */).then((openPods, err) => {
       if (err || !openPods) {
-        console.log(`LoadBalancer: Error! Couldn't find ${openPodsKey} openPodsKey in Redis:`, err);
+        if (logDebug) console.log(`LOADBALANCER.selectSlackChannel: ERROR finding openPodsKey ${openPodsKey} in Redis: err || !openPods`);
         return null;
       }
+      if (logDebug) console.log(`LOADBALANCER.selectSlackChannel: Successfully found openPods with openPodsKey ${openPodsKey} in Redis: ${JSON.stringify(openPods)}`);
 
       numVoters = parseInt(numVoters);
-      const selectedPodNumber = numVoters % openPods.length;
-      const selectedChannelName = openPods[selectedPodNumber];
 
+      const selectedPodNumber = numVoters % openPods.length;
+      if (logDebug) console.log(`LOADBALANCER.selectSlackChannel: selectedPodNumber = numVoters % openPods.length = ${numVoters} % ${openPods.length} = ${selectedPodNumber}`);
+
+      const selectedChannelName = openPods[selectedPodNumber];
+      if (logDebug) console.log(`LOADBALANCER.selectSlackChannel: selectedChannelName = openPods[selectedPodNumber] = ${openPods}[${selectedPodNumber}] = ${selectedChannelName}`);
+
+      if (logDebug) console.log(`LOADBALANCER.selectSlackChannel: Updating Redis voterCounterKey ${voterCounterKey} from ${numVoters} to ${numVoters + 1}`);
       redisClient.setAsync(voterCounterKey, numVoters + 1);
 
+      if (logDebug) console.log(`Exiting LOADBALANCER.selectSlackChannel with return value: ${selectedChannelName}`);
       return new Promise(resolve => resolve(selectedChannelName));
-    }).catch(err => console.log("LoadBalancer: Error selecting a channel:", err));
-  }).catch(err => console.log("LoadBalancer: Error selecting a channel:", err));;
+    }).catch(err => {
+      if (logDebug) console.log("LoadBalancer: Error selecting a channel:", err);
+    });
+  }).catch(err => {
+    if (logDebug) console.log("LoadBalancer: Error selecting a channel:", err);
+  });
 };
--- server.js
+++ server.js
@@ -35,12 +35,17 @@ bluebird.promisifyAll(redis);
 var redisClient = redis.createClient(process.env.REDISCLOUD_URL);
 
 const handleIncomingTwilioMessage = (req, entryPoint) => {
-  console.log(`Receiving Twilio message from ${entryPoint} entry point voter.`);
+  console.log("\nEntering SERVER.handleIncomingTwilioMessage");
   const userPhoneNumber = req.body.From;
   const twilioPhoneNumber = req.body.To;
   const userMessage = req.body.Body;
   const MD5 = new Hashes.MD5;
   const userId = MD5.hex(userPhoneNumber);
+  console.log(`SERVER.handleIncomingTwilioMessage: Receiving Twilio message from ${entryPoint} entry point voter,
+                userPhoneNumber: ${userPhoneNumber},
+                twilioPhoneNumber: ${twilioPhoneNumber},
+                userMessage: ${userMessage},
+                userId: ${userId}`);
 
   const inboundDbMessageEntry = DbApiUtil.populateIncomingDbMessageTwilioEntry({
     userMessage,
@@ -52,39 +57,45 @@ const handleIncomingTwilioMessage = (req, entryPoint) => {
 
   const redisHashKey = `${userId}:${twilioPhoneNumber}`;
 
+  console.log(`SERVER.handleIncomingTwilioMessage (${userId}): Retrieving userInfo using redisHashKey: ${redisHashKey}`);
+
   RedisApiUtil.getHash(redisClient, redisHashKey).then(userInfo => {
+    console.log(`SERVER.handleIncomingTwilioMessage (${userId}): Successfully received Redis response for userInfo retrieval with redisHashKey ${redisHashKey}, userInfo: ${JSON.stringify(userInfo)}`);
     // Seen this voter before
     if (userInfo != null) {
+      console.log(`SERVER.handleIncomingTwilioMessage (${userId}): Voter is known to us (Redis returned userInfo for redisHashKey ${redisHashKey})`);
       // PUSH
       if (entryPoint === LoadBalancer.PUSH_ENTRY_POINT) {
-        console.log("Server: handleClearedVoter");
+        console.log(`SERVER.handleIncomingTwilioMessage (${userId}): Skipping automated system since entrypoint is ${LoadBalancer.PUSH_ENTRY_POINT}.`);
         // Don't do dislcaimer or U.S. state checks for push voters.
         Router.handleClearedVoter({userInfo, userPhoneNumber, userMessage}, redisClient, twilioPhoneNumber, inboundDbMessageEntry);
       // PULL
       } else if (userInfo.confirmedDisclaimer) {
+        console.log(`SERVER.handleIncomingTwilioMessage (${userId}): Activating automated system since entrypoint is ${LoadBalancer.PULL_ENTRY_POINT}.`);
+        console.log(`SERVER.handleIncomingTwilioMessage (${userId}): Voter has previously confirmed the disclaimer.`);
         // Voter has a state determined. The U.S. state name is used for
         // operator messages as well as to know whether a U.S. state is known
         // for the voter. This may not be ideal (create separate bool?).
         // If a volunteer has intervened, turn off automated replies.
-        if (!userInfo.stateName && userInfo.volunteerEngaged) console.log("Server: No U.S. state for voter but volunteer engaged, so disabling automated replies.")
         if (userInfo.stateName || userInfo.volunteerEngaged) {
-          console.log("Server: handleClearedVoter");
+          console.log(`SERVER.handleIncomingTwilioMessage (${userId}): Known U.S. state for voter (${userInfo.stateName}) or volunteer has engaged (${userInfo.volunteerEngaged}). Automated system no longer active.`);
           Router.handleClearedVoter({userInfo, userPhoneNumber, userMessage}, redisClient, twilioPhoneNumber, inboundDbMessageEntry);
         // Voter has no state determined
         } else {
-          console.log("Server: determineVoterState");
+          console.log(`SERVER.handleIncomingTwilioMessage (${userId}): U.S. state for voter is not known. Automated system will attempt to determine.`);
           Router.determineVoterState({userInfo, userPhoneNumber, userMessage}, redisClient, twilioPhoneNumber, inboundDbMessageEntry);
         }
       } else {
-        console.log("Server: handleDisclaimer");
+        console.log(`SERVER.handleIncomingTwilioMessage (${userId}): Activating automated system since entrypoint is ${LoadBalancer.PULL_ENTRY_POINT}`);
+        console.log(`SERVER.handleIncomingTwilioMessage (${userId}): Voter has NOT previously confirmed the disclaimer. Automated system will attempt to confirm.`);
         Router.handleDisclaimer({userInfo, userPhoneNumber, userMessage}, redisClient, twilioPhoneNumber, inboundDbMessageEntry);
       }
     // Haven't seen this voter before
     } else {
-      console.log("Server: handleNewVoter");
+      console.log(`SERVER.handleIncomingTwilioMessage (${userId}): Voter is new to us (Redis returned no userInfo for redisHashKey ${redisHashKey})`);
       Router.handleNewVoter({userPhoneNumber, userMessage, userId}, redisClient, twilioPhoneNumber, inboundDbMessageEntry, entryPoint);
     }
-  });
+  }).catch(err => console.log(`SERVER.handleIncomingTwilioMessage (${userId}): ERROR looking up userInfo in Redis with redisHashKey ${redisHashKey}, error: ${err}`));
 };
 
 app.post('/push', (req, res) => {
@@ -114,6 +125,9 @@ app.post('/push', (req, res) => {
 });
 
 app.post('/twilio-push', (req, res) => {
+  console.log("\n\n**************************************************************************************************");
+  console.log("******************************************************************************************************");
+  console.log("Entering SERVER POST /twilio-push");
   const twiml = new MessagingResponse();
   handleIncomingTwilioMessage(req, LoadBalancer.PUSH_ENTRY_POINT);
 
@@ -123,6 +137,9 @@ app.post('/twilio-push', (req, res) => {
 
 
 app.post('/twilio-pull', (req, res) => {
+  console.log("\n\n**************************************************************************************************");
+  console.log("******************************************************************************************************");
+  console.log("Entering SERVER POST /twilio-pull");
   const twiml = new MessagingResponse();
   handleIncomingTwilioMessage(req, LoadBalancer.PULL_ENTRY_POINT);
 
@@ -158,31 +175,36 @@ const isRetry = (req) => {
 };
 
 app.post('/slack', upload.array(), (req, res) => {
+  console.log("\n\n**************************************************************************************************");
+  console.log("******************************************************************************************************");
+  console.log("Entering SERVER POST /slack");
   console.log(JSON.stringify(req.headers));
   res.type('application/json');
 
   if (!req.body.challenge) {
     const reqBody = req.body;
     if(process.env.NODE_ENV !== "development" && !passesAuth(req)) {
-      console.log('doesnt pass auth');
+      console.log("SERVER POST /slack: ERROR in authenticating /slack request is from Slack.");
       res.sendStatus(401);
       return;
     }
     res.sendStatus(200);
-    console.log('Passes Slack auth');
     if (reqBody.event.type === "message"
         && reqBody.event.user != process.env.SLACK_BOT_USER_ID) {
+      console.log(`SERVER POST /slack: Slack event listener caught non-bot Slack message from ${reqBody.event.user}.`);
       const redisHashKey = `${reqBody.event.channel}:${reqBody.event.thread_ts}`;
 
       // Pass Slack message to Twilio
       RedisApiUtil.getHash(redisClient, redisHashKey).then(redisData => {
         if (redisData != null) {
+          console.log("SERVER POST /slack: Server received non-bot Slack message INSIDE a voter thread.");
           SlackApiUtil.fetchSlackUserName(reqBody.event.user).then(originatingSlackUserName => {
+            console.log(`SERVER POST /slack: Successfully determined Slack user name of message sender: ${originatingSlackUserName}, from Slack user ID: ${reqBody.event.user}`);
             Router.handleSlackVoterThreadMessage(req, redisClient, redisData, originatingSlackUserName);
-          });
+          }).catch(err => console.log(`SERVER POST /slack: ERROR determining Slack user name from Slack user ID`, err));
         } else {
           // Hash doesn't exist (this message is likely outside of a voter thread).
-          console.log("Server received Slack message outside a voter thread.")
+          console.log("SERVER POST /slack: Server received non-bot Slack message OUTSIDE a voter thread. Doing nothing.");
         }
       });
     } else if (reqBody.event.type === "app_mention"
@@ -190,14 +212,18 @@ app.post('/slack', upload.array(), (req, res) => {
                 && reqBody.authed_users[0] === process.env.SLACK_BOT_USER_ID
                 // Require that the message was sent in the #admin-control-room Slack channel
                 && reqBody.event.channel == process.env.ADMIN_CONTROL_ROOM_SLACK_CHANNEL_ID) {
+      console.log("SERVER POST /slack: Slack event listener caught bot mention in admin channel.");
       SlackApiUtil.fetchSlackUserName(reqBody.event.user).then(originatingSlackUserName => {
-        console.log(`Received admin control command from ${originatingSlackUserName}: ${reqBody.event.text}`);
+        console.log(`SERVER POST /slack: Successfully determined Slack user name of bot mentioner: ${originatingSlackUserName}, from Slack user ID: ${reqBody.event.user}`);
+        console.log(`SERVER POST /slack: Received admin control command from ${originatingSlackUserName}: ${reqBody.event.text}`);
         Router.handleSlackAdminCommand(reqBody, redisClient, originatingSlackUserName);
       });
     }
   } else {
+    console.log("SERVER POST /slack: Authenticating Slack bot event listener with Node server.");
     // Authenticate Slack connection to Heroku.
     if (SlackApiUtil.authenticateConnectionToSlack(req.body.token)) {
+      console.log("SERVER POST /slack: Slack-Node authentication successful.");
       res.status(200).json({ challenge: req.body.challenge });
     }
   }
