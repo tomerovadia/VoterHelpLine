b5685294f4b3a246bebe0560781b82cd4e4d95fc
Tomer Ovadia <tomertomertomer@gmail.com>
2020-09-13 17:37:51 -0400

    Put /push response at top

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ const handleIncomingTwilioMessage = (req, entryPoint) => {
 };
 
 app.post('/push', (req, res) => {
+  res.status(200);
   const TWILIO_PHONE_NUMBER = "+18557041009";
   const MESSAGE = "This is Voter Help Line! We sent you an absentee ballot request form. Did you receive it? Text STOP to stop messages. Msg & data rates may apply."
 
@@ -131,8 +132,6 @@ app.post('/push', (req, res) => {
       delay += INTERVAL_MILLISECONDS;
     }
   });
-
-  res.status(200);
 });
 
 app.post('/twilio-push', (req, res) => {
