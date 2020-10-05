098d56a560700a20eb983f819dda75ea47e9fff0
Tomer Ovadia <tomertomertomer@gmail.com>
2020-10-05 00:53:44 -0400

    Add redis.ts to external_config directory with initializeOpenPods function to initialize open pods in Redis

---
+++ external_config/redis.ts
@@ -0,0 +1,27 @@
+import { PromisifiedRedisClient } from '../src/redis_client';
+import { getStateConstants } from '../src/state_constants';
+
+// This function establishes initial open pods in Redis,
+// populating demo and non-demo entry points and
+// defaulting to the 0th open pod per entry point.
+export async function initializeOpenPods(
+  redisClient: PromisifiedRedisClient
+): Promise<void> {
+  const stateNames = Object.values(getStateConstants());
+  stateNames.push('National');
+  for (const idx in stateNames) {
+    const stateName = stateNames[idx];
+    const stateNameNoSpaces = stateName.replace(/\s/g, '');
+    const stateNameHyphens = stateName.toLowerCase().replace(/\s/g, '-');
+
+    const openPodsKey = `openPodsPull${stateNameNoSpaces}`;
+    const openPodsValue = `${stateNameHyphens}-0`;
+    await redisClient.delAsync(openPodsKey);
+    await redisClient.rpushAsync(openPodsKey, [openPodsValue]);
+
+    const openPodsDemoKey = `openPodsPullDemo${stateNameNoSpaces}`;
+    const openPodsDemoValue = `demo-${stateNameHyphens}-0`;
+    await redisClient.delAsync(openPodsDemoKey);
+    await redisClient.rpushAsync(openPodsDemoKey, [openPodsDemoValue]);
+  }
+}
