tomerovadia/VoterHelpLine

commit 2999111c77fb6125fde45399203cfba7689ed98b

show verbatim command as first line of result

src/slack_interaction_handler.ts
@@ -395,6 +395,8 @@ export async function handleCommandUnclaimed(
     arg = '';
   }
 
+  const lines = ['`/unclaimed' + (arg ? ` ${arg}` : '') + '`'];
+
   const slackChannelIds = arg
     ? await RedisApiUtil.getHash(redisClient, 'slackPodChannelIds')
     : {};
@@ -423,10 +425,10 @@ export async function handleCommandUnclaimed(
   const threads = await DbApiUtil.getUnclaimedVoters(
     arg === '*' ? null : showChannelId
   );
-  const lines: string[] = [
+  lines.push(
     `${threads.length} unclaimed voters` +
-      (arg === '*' ? ' in all channels' : ''),
-  ];
+      (arg === '*' ? ' in all channels' : '')
+  );
 
   for (const thread of threads) {
     const messageTs =
@@ -507,7 +509,7 @@ export async function handleCommandNeedsAttention(
     arg = '';
   }
 
-  let lines = [] as string[];
+  let lines = ['`/needs-attention' + (arg ? ` ${arg}` : '') + '`'];
 
   // Which user we'll show voters for (if the command arg doesn't have us show * or a channel)
   // Empty arg means current user.

commit 6da38a49f7c56f0ee8ecc41a5e210ace7401f536

I broke this with 2999111c77fb6125fde45399203cfba7689ed98b

src/slack_interaction_handler.ts
@@ -655,7 +655,7 @@ export async function handleCommandNeedsAttention(
     );
   }
 
-  if (lines.length == 0) {
+  if (lines.length == 1) {
     // For a single user
     const ulines = await getNeedsAttentionList(showUserId);
     lines.push(