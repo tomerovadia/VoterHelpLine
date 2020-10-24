5b72627ef102da2fec20ba4067cdc7ab37244bea
Sage Weil <sage@newdream.net>
2020-10-23 20:22:34 -0500

    remove extra 'the' from welcome_state

--- src/message_constants.ts
+++ src/message_constants.ts
@@ -72,7 +72,7 @@ export function CLARIFY_HELPLINE_REQUEST(): string {
 
 // Only used by VOTE_AMERICA.
 export function WELCOME_AND_STATE_QUESTION(): string {
-  return "Welcome to the VoteAmerica! Msg&data rates may apply.\n\nReply VOTED if you've already voted, STOP to unsubscribe.\n\nIn which U.S. state are you looking to vote?";
+  return "Welcome to VoteAmerica! Msg&data rates may apply.\n\nReply VOTED if you've already voted, STOP to unsubscribe.\n\nIn which U.S. state are you looking to vote?";
 }
 
 export function VOTED_WELCOME_RESPONSE(): string {
