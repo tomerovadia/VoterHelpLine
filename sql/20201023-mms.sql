56ee00013e545adc3f73ec72ea4505739af66a4f
Sage Weil <sage@newdream.net>
2020-10-23 19:00:40 -0500

    typos in mms migration

--- sql/20201023-mms.sql
+++ sql/20201023-mms.sql
@@ -1,4 +1,4 @@
--- Store the array of slack_files as a jsonb colum. We use jsonb rather than
+-- Store the array of slack_attachments as a jsonb column. We use jsonb rather than
 -- a text array because they have similar performance, but JSON is easier to
 -- sync to a Redshift data warehouse (e.g. Civis)
 ALTER TABLE messages
