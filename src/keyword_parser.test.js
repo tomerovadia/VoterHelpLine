0fc3881034fc329438c5ed3e9904d220b48f12ec
Tomer Ovadia <tomertomertomer@gmail.com>
2020-10-25 19:26:25 -0400

    Change STOP logic

--- src/keyword_parser.test.js
+++ src/keyword_parser.test.js
@@ -1,41 +1,41 @@
 const KeywordParser = require('./keyword_parser');
 
 test('Does not consider a normal message to contain STOP keyword.', () => {
-  expect(KeywordParser.containsStopKeyword('Some normal message')).toBe(false);
+  expect(KeywordParser.isStopKeyword('Some normal message')).toBe(false);
 });
 
 test('Detects STOP keyword.', () => {
-  expect(KeywordParser.containsStopKeyword('STOP')).toBe(true);
+  expect(KeywordParser.isStopKeyword('STOP')).toBe(true);
 });
 
 test('Detects STOP keyword with spaces on either side.', () => {
-  expect(KeywordParser.containsStopKeyword('  STOP   ')).toBe(true);
+  expect(KeywordParser.isStopKeyword('  STOP   ')).toBe(true);
 });
 
 test('Does not detect STOP keyword with spaces within the word.', () => {
   expect(
-    KeywordParser.containsStopKeyword('VOTING IS TOP OF MIND FOR ME YAY')
+    KeywordParser.isStopKeyword('VOTING IS TOP OF MIND FOR ME YAY')
   ).toBe(false);
 });
 
 test('Detects STOP keyword with punctuation.', () => {
-  expect(KeywordParser.containsStopKeyword('STOP!')).toBe(true);
+  expect(KeywordParser.isStopKeyword('STOP!')).toBe(true);
 });
 
 test('Detects STOP keyword in any upper/lower case.', () => {
-  expect(KeywordParser.containsStopKeyword('stop')).toBe(true);
-  expect(KeywordParser.containsStopKeyword('StOp')).toBe(true);
+  expect(KeywordParser.isStopKeyword('stop')).toBe(true);
+  expect(KeywordParser.isStopKeyword('StOp')).toBe(true);
 });
 
 test('Detects STOP keyword alone in any upper/lower case even with punctuation.', () => {
-  expect(KeywordParser.containsStopKeyword('stop!!')).toBe(true);
-  expect(KeywordParser.containsStopKeyword('Stop!!')).toBe(true);
-  expect(KeywordParser.containsStopKeyword('S.T.O.P.')).toBe(true);
+  expect(KeywordParser.isStopKeyword('stop!!')).toBe(true);
+  expect(KeywordParser.isStopKeyword('Stop!!')).toBe(true);
+  expect(KeywordParser.isStopKeyword('S.T.O.P.')).toBe(true);
 });
 
 test('Does not detect lowercase stop in the context of a longer message if lowercase.', () => {
   expect(
-    KeywordParser.containsStopKeyword(
+    KeywordParser.isStopKeyword(
       "I'll stop by the polling place on the way to work"
     )
   ).toBe(false);
