78367bad79ed5a050a63403198b96b7854a67511
Sage Weil <sage@newdream.net>
2020-10-28 20:58:12 -0500

    simplify migration

--- sql/20201027-is-demo.sql
+++ sql/20201027-is-demo.sql
@@ -2,15 +2,9 @@
 ALTER TABLE threads
 ADD COLUMN is_demo boolean;
 
-WITH demo_number AS (
-    SELECT DISTINCT(to_phone_number) as phone
-    FROM messages
-    WHERE is_demo = true AND direction='INBOUND'
-), demo_channel AS (
-    SELECT DISTINCT(slack_channel) FROM messages
-    WHERE
-        EXISTS(SELECT 1 from demo_number WHERE to_phone_number = phone)
-        OR EXISTS(SELECT 1 from demo_number WHERE from_phone_number = phone)
+
+WITH demo_channel AS (
+    SELECT DISTINCT(slack_channel) FROM messages WHERE is_demo=true
 )
 UPDATE threads
 SET is_demo = true
@@ -18,15 +12,8 @@ WHERE EXISTS (
     SELECT 1 FROM demo_channel d WHERE threads.slack_channel_id = d.slack_channel
 );
 
-WITH demo_number AS (
-    SELECT DISTINCT(to_phone_number) as phone
-    FROM messages
-    WHERE is_demo = true AND direction='INBOUND'
-), demo_channel AS (
-    SELECT DISTINCT(slack_channel) FROM messages
-    WHERE
-        EXISTS(SELECT 1 from demo_number WHERE to_phone_number = phone)
-        OR EXISTS(SELECT 1 from demo_number WHERE from_phone_number = phone)
+WITH demo_channel AS (
+    SELECT DISTINCT(slack_channel) FROM messages WHERE is_demo=true
 )
 UPDATE threads
 SET is_demo = false
