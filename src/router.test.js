13141c7a9e93a56bf0f4b90669d18eaa9ef709f5
Tomer Ovadia <tomertomertomer@gmail.com>
2020-11-03 09:05:53 -0500

    Fix router unit tests

--- src/router.test.js
+++ src/router.test.js
@@ -736,7 +736,7 @@ describe('determineVoterState', () => {
       ).then(() => {
         expect(TwilioApiUtil.sendMessage.mock.calls[0][0]).toEqual(
           expect.stringMatching(
-            /Great!.*We try to reply within minutes but may take 24 hours./i
+            /Great!.*will be with you shortly/i
           )
         );
         expect(TwilioApiUtil.sendMessage.mock.calls[0][1]).toEqual(
@@ -833,7 +833,7 @@ describe('determineVoterState', () => {
         expectNthSlackMessageToChannel(
           'CTHELOBBYID',
           1,
-          ['We try to reply within minutes but may take 24 hours.'],
+          ['will be with you shortly'],
           '293874928374'
         );
       });
