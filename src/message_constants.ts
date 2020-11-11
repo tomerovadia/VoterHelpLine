4b955cb40b1f55dc6650487ea5ede652d7772f06
Sage Weil <sage@newdream.net>
2020-11-11 10:40:07 -0600

    handle VOTED on active thread; reply only if idle

--- src/message_constants.ts
+++ src/message_constants.ts
@@ -79,6 +79,10 @@ export function VOTED_WELCOME_RESPONSE(): string {
   return 'Thank you for voting!\n\nReply STOP to opt out of texts. Msg&data rates may apply.';
 }
 
+export function VOTED_RESPONSE(): string {
+  return 'Thank you for voting!';
+}
+
 export function STATE_QUESTION(): string {
   switch (process.env.CLIENT_ORGANIZATION) {
     case 'VOTE_AMERICA':
