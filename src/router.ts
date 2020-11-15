eaed3e1f837487dc8240fcebc04942190ec564df
Sage Weil <sage@newdream.net>
2020-11-14 19:34:15 -0600

    route a voter => volunteerEngaged
    Otherwise, a fresh voter could get routed by a volunteer to a state
    channel, and if the next message is from the voter it will get
    interpreted as a state attempt.
    
    Don't put this in routeVoterToSlackChannel() since one caller is
    automated and setting this value doesn't make sense in that case.

--- src/router.ts
+++ src/router.ts
@@ -1554,6 +1554,8 @@ export async function handleSlackVoterThreadMessage(
       for (const name in slackChannelIds) {
         slackChannelNames[slackChannelIds[name]] = name;
       }
+      // Mark that a volunteer has engaged (by routing them!)
+      userInfo.volunteerEngaged = true;
       await routeVoterToSlackChannel(
         userInfo,
         redisClient,
@@ -1908,6 +1910,8 @@ export async function handleSlackAdminCommand(
           logger.debug(
             `Router.handleSlackAdminCommand: Routing voter from ${userInfo.activeChannelName} to ${adminCommandArgs.destinationSlackChannelName}.`
           );
+          // Mark that a volunteer has engaged (by routing them!)
+          userInfo.volunteerEngaged = true;
           await routeVoterToSlackChannel(
             userInfo,
             redisClient,
