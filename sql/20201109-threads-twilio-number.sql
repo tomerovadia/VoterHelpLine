7029166dbf97002e5422f26d9666cbe9e069eef9
Sage Weil <sage@newdream.net>
2020-11-30 19:01:49 -0600

    Fix migration when adding twilio_phone_number to threads table

--- sql/20201109-threads-twilio-number.sql
+++ sql/20201109-threads-twilio-number.sql
@@ -5,6 +5,11 @@ ADD COLUMN twilio_phone_number varchar(12);
 --- populate it (run this *after* the new code deploys if under load)
 UPDATE threads
 SET twilio_phone_number=(
-    SELECT from_phone_number FROM messages WHERE is_demo=threads.is_demo ORDER BY twilio_receive_timestamp DESC limit 1
+    SELECT from_phone_number FROM messages
+    WHERE
+        is_demo=threads.is_demo
+        AND to_phone_number=threads.user_phone_number
+        AND direction='OUTBOUND'
+    ORDER BY twilio_receive_timestamp DESC limit 1
 )
 ;
